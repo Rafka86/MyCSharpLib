ea381aba399167185ed0453290504c4647d119e0
Rafka <fw.ge-mu.930@jb4.so-net.ne.jp>
2017-06-28 16:11:57 +0900

    Convert tab to space.

--- Librafka/MathLib/Converter.cs
+++ Librafka/MathLib/Converter.cs
@@ -2,96 +2,96 @@
 using System.Collections.Generic;
 
 namespace Librafka.MathLib {
-	internal enum CharacterType { Num, Alp, Opr, Brc }
+  internal enum CharacterType { Num, Alp, Opr, Brc }
 
-	internal enum StringType { Fnc, Cns, Var }
+  internal enum StringType { Fnc, Cns, Var }
 
-	internal static class Converter {
-		private static readonly Stack<Token> Mem = new Stack<Token>();
+  internal static class Converter {
+    private static readonly Stack<Token> Mem = new Stack<Token>();
 
-		private static CharacterType GetCharType(char c) {
-			if ('0' <= c && c <= '9') return CharacterType.Num;
-			if ('A' <= c && c <= 'Z' || 'a' <= c && c <= 'z') return CharacterType.Alp;
-			switch (c) {
-				case '(': case ')': return CharacterType.Brc;
-				case '+': case '-': case '*': case '/': case '^': case '%': return CharacterType.Opr;
-				case '.': return CharacterType.Num;
-				default: throw new ArgumentOutOfRangeException(nameof(c),"数式として使用できない文字です．");
-			}
-		}
+    private static CharacterType GetCharType(char c) {
+      if ('0' <= c && c <= '9') return CharacterType.Num;
+      if ('A' <= c && c <= 'Z' || 'a' <= c && c <= 'z') return CharacterType.Alp;
+      switch (c) {
+        case '(': case ')': return CharacterType.Brc;
+        case '+': case '-': case '*': case '/': case '^': case '%': return CharacterType.Opr;
+        case '.': return CharacterType.Num;
+        default: throw new ArgumentOutOfRangeException(nameof(c),"数式として使用できない文字です．");
+      }
+    }
 
-		private static StringType GetStringType(string s) {
-			switch (s) {
-				case "SIN": case "COS": case "TAN": case "SINH": case "COSH": case"TANH": case "LOG": case "EXP": return StringType.Fnc;
-				case "PI": case "E": case "EPS": return StringType.Cns;
-				default: return StringType.Var;
-			}
-		}
+    private static StringType GetStringType(string s) {
+      switch (s) {
+        case "SIN": case "COS": case "TAN": case "SINH": case "COSH": case"TANH": case "LOG": case "EXP": return StringType.Fnc;
+        case "PI": case "E": case "EPS": return StringType.Cns;
+        default: return StringType.Var;
+      }
+    }
 
-		internal static Queue<Token> Tokenize(string s) {
-			var expr = new Queue<Token>();
+    internal static Queue<Token> Tokenize(string s) {
+      var expr = new Queue<Token>();
 
-			expr.Clear();
+      expr.Clear();
 
-			for (var i = 0; i < s.Length; i++) {
-				int si;
-				switch (GetCharType(s[i])) {
-					case CharacterType.Num:
-						si = i;
-						while (i < s.Length && (GetCharType(s[i]) == CharacterType.Num || s[i] == '.')) i++;
-						if (i < s.Length && GetCharType(s[i]) != CharacterType.Opr) s.Insert(i, "*");
-						expr.Enqueue(new Token(TokenType.Num, s.Substring(si, i - si)));
-						i--; break;
-					case CharacterType.Alp:
-						si = i;
-						while (i < s.Length && (GetCharType(s[i]) == CharacterType.Num || GetCharType(s[i]) == CharacterType.Alp)) i++;
-						if (i < s.Length && GetCharType(s[i]) != CharacterType.Opr) s.Insert(i, "*");
-						var buf = s.Substring(si, i - si);
-						switch (GetStringType(buf.ToUpper())) {
-							case StringType.Fnc: expr.Enqueue(new Token(TokenType.Fnc, buf)); break;
-							case StringType.Cns: expr.Enqueue(new Token(TokenType.Cns, buf)); break;
-							case StringType.Var: break;
-							default: expr.Enqueue(new Token(TokenType.Var, buf)); break;
-						}
-						i--; break;
-					case CharacterType.Opr:
-						if ((i == 0 || s[i - 1] == '(') && s[i] == '-') expr.Enqueue(new Token(TokenType.Opr, "_"));
-						else expr.Enqueue(new Token(TokenType.Opr, s.Substring(i, 1)));
-						break;
-					case CharacterType.Brc: expr.Enqueue(new Token(TokenType.Brc, s.Substring(i, 1))); break;
-					default: throw new ArgumentOutOfRangeException();
-				}
-			}
+      for (var i = 0; i < s.Length; i++) {
+        int si;
+        switch (GetCharType(s[i])) {
+          case CharacterType.Num:
+            si = i;
+            while (i < s.Length && (GetCharType(s[i]) == CharacterType.Num || s[i] == '.')) i++;
+            if (i < s.Length && GetCharType(s[i]) != CharacterType.Opr) s.Insert(i, "*");
+            expr.Enqueue(new Token(TokenType.Num, s.Substring(si, i - si)));
+            i--; break;
+          case CharacterType.Alp:
+            si = i;
+            while (i < s.Length && (GetCharType(s[i]) == CharacterType.Num || GetCharType(s[i]) == CharacterType.Alp)) i++;
+            if (i < s.Length && GetCharType(s[i]) != CharacterType.Opr) s.Insert(i, "*");
+            var buf = s.Substring(si, i - si);
+            switch (GetStringType(buf.ToUpper())) {
+              case StringType.Fnc: expr.Enqueue(new Token(TokenType.Fnc, buf)); break;
+              case StringType.Cns: expr.Enqueue(new Token(TokenType.Cns, buf)); break;
+              case StringType.Var: break;
+              default: expr.Enqueue(new Token(TokenType.Var, buf)); break;
+            }
+            i--; break;
+          case CharacterType.Opr:
+            if ((i == 0 || s[i - 1] == '(') && s[i] == '-') expr.Enqueue(new Token(TokenType.Opr, "_"));
+            else expr.Enqueue(new Token(TokenType.Opr, s.Substring(i, 1)));
+            break;
+          case CharacterType.Brc: expr.Enqueue(new Token(TokenType.Brc, s.Substring(i, 1))); break;
+          default: throw new ArgumentOutOfRangeException();
+        }
+      }
 
-			return expr;
-		}
+      return expr;
+    }
 
-		internal static void ToRpn(Queue<Token> expr) {
-			var n = expr.Count;
+    internal static void ToRpn(Queue<Token> expr) {
+      var n = expr.Count;
 
-			Mem.Clear();
+      Mem.Clear();
 
-			for (var i = 0; i < n; i++) {
-				var t = expr.Dequeue();
-				switch (t.Identity) {
-					case TokenType.Num:	case TokenType.Cns:	case TokenType.Var:	expr.Enqueue(t); break;
-					case TokenType.Fnc:	Mem.Push(t); break;
-					case TokenType.Brc:
-						if (t.Buffer == "(") Mem.Push(t);
-						if (t.Buffer == ")") {
-							while (Mem.Peek().Buffer != "(") expr.Enqueue(Mem.Pop());
-							if (Mem.Count != 0 && Mem.Peek().Identity == TokenType.Fnc) expr.Enqueue(Mem.Pop());
-						} break;
-					case TokenType.Opr:
-						if (Mem.Count == 0) { Mem.Push(t); break; }
-						while (Mem.Count != 0 && Mem.Peek().Identity == TokenType.Opr) 
-							if ((t.LeftCat && t.Priority >= Mem.Peek().Priority) || t.Priority > Mem.Peek().Priority) expr.Enqueue(Mem.Pop());
-							else break;
-						Mem.Push(t); break;
-					default: throw new ArgumentOutOfRangeException();
-				}
-			}
-			while (Mem.Count != 0) expr.Enqueue(Mem.Pop());
-		}
-	}
+      for (var i = 0; i < n; i++) {
+        var t = expr.Dequeue();
+        switch (t.Identity) {
+          case TokenType.Num:	case TokenType.Cns:	case TokenType.Var:	expr.Enqueue(t); break;
+          case TokenType.Fnc:	Mem.Push(t); break;
+          case TokenType.Brc:
+            if (t.Buffer == "(") Mem.Push(t);
+            if (t.Buffer == ")") {
+              while (Mem.Peek().Buffer != "(") expr.Enqueue(Mem.Pop());
+              if (Mem.Count != 0 && Mem.Peek().Identity == TokenType.Fnc) expr.Enqueue(Mem.Pop());
+            } break;
+          case TokenType.Opr:
+            if (Mem.Count == 0) { Mem.Push(t); break; }
+            while (Mem.Count != 0 && Mem.Peek().Identity == TokenType.Opr) 
+              if ((t.LeftCat && t.Priority >= Mem.Peek().Priority) || t.Priority > Mem.Peek().Priority) expr.Enqueue(Mem.Pop());
+              else break;
+            Mem.Push(t); break;
+          default: throw new ArgumentOutOfRangeException();
+        }
+      }
+      while (Mem.Count != 0) expr.Enqueue(Mem.Pop());
+    }
+  }
 }
--- Librafka/MathLib/Expression.cs
+++ Librafka/MathLib/Expression.cs
@@ -3,122 +3,122 @@ using System.Text;
 using System.Collections.Generic;
 
 namespace Librafka.MathLib {
-	/// <summary>
-	/// 数式を表すクラスである．
-	/// 文字列で表された数式を計算できる形に解釈し，
-	/// 任意のタイミングで任意の回数計算を行うことができる．
-	/// </summary>
-	public class Expression {
-		private Queue<Token> _expr;
-		private string _stringExp;
+  /// <summary>
+  /// 数式を表すクラスである．
+  /// 文字列で表された数式を計算できる形に解釈し，
+  /// 任意のタイミングで任意の回数計算を行うことができる．
+  /// </summary>
+  public class Expression {
+    private Queue<Token> _expr;
+    private string _stringExp;
 
-		/// <summary>
-		/// 数式クラスのコンストラクタ．
-		/// 文字列で表された数式を計算できる形に解釈する．
-		/// </summary>
-		/// <param name="exp">数式を表す文字列．</param>
-		public Expression(string exp) {
-			_stringExp = exp;
-			_expr = Converter.Tokenize(exp.Replace(" ", ""));
-			Converter.ToRpn(_expr);
-		}
+    /// <summary>
+    /// 数式クラスのコンストラクタ．
+    /// 文字列で表された数式を計算できる形に解釈する．
+    /// </summary>
+    /// <param name="exp">数式を表す文字列．</param>
+    public Expression(string exp) {
+      _stringExp = exp;
+      _expr = Converter.Tokenize(exp.Replace(" ", ""));
+      Converter.ToRpn(_expr);
+    }
 
-		/// <summary>
-		/// 数式を計算し，値を求める．
-		/// </summary>
-		/// <param name="vars">式に含まれる変数に代入するための値の情報．</param>
-		/// <returns>計算によって求められた値．</returns>
-		public double Eval(Dictionary<string, double> vars) {
-			var ans = new Stack<double>();
+    /// <summary>
+    /// 数式を計算し，値を求める．
+    /// </summary>
+    /// <param name="vars">式に含まれる変数に代入するための値の情報．</param>
+    /// <returns>計算によって求められた値．</returns>
+    public double Eval(Dictionary<string, double> vars) {
+      var ans = new Stack<double>();
 
-			for (var i = 0; i < _expr.Count; i++) {
-				var t = _expr.Dequeue();
-				switch(t.Identity){
-					case TokenType.Num: ans.Push(double.Parse(t.Buffer)); break;
-					case TokenType.Var:
-						if (vars?.ContainsKey(t.Buffer) ?? false) ans.Push(vars[t.Buffer]);
-						else throw new ArgumentOutOfRangeException("vars", "計算に必要な変数の値の情報が不足しています．");
-						break;
-					case TokenType.Opr:
-						var p = ans.Pop();
-						switch (t.Buffer) {
-							case "+": ans.Push(ans.Pop() + p); break;
-							case "-": ans.Push(ans.Pop() - p); break;
-							case "*": ans.Push(ans.Pop() * p); break;
-							case "/": ans.Push(ans.Pop() / p); break;
-							case "%": ans.Push(ans.Pop() % p); break;
-							case "^": ans.Push(Math.Pow(ans.Pop(), p)); break;
-							case "_": ans.Push(-p); break;
-						} break;
-					case TokenType.Cns:
-						switch(t.Buffer.ToUpper()) {
-							case "PI": ans.Push(Math.PI); break;
-							case "E": ans.Push(Math.E); break;
-							case "EPS": ans.Push(double.Epsilon); break;
-						} break;
-					case TokenType.Fnc:
-						switch(t.Buffer) {
-							case "sin": case "SIN": ans.Push(Math.Sin(ans.Pop())); break;
-							case "cos": case "COS": ans.Push(Math.Cos(ans.Pop())); break;
-							case "tan": case "TAN": ans.Push(Math.Tan(ans.Pop())); break;
-							case "Sin": ans.Push(Math.Asin(ans.Pop())); break;
-							case "Cos": ans.Push(Math.Acos(ans.Pop())); break;
-							case "Tan": ans.Push(Math.Atan(ans.Pop())); break;
-							case "sinh": case "Sinh": case "SINH": ans.Push(Math.Sinh(ans.Pop())); break;
-							case "cosh": case "Cosh": case "COSH": ans.Push(Math.Cosh(ans.Pop())); break;
-							case "tanh": case "Tanh": case "TANH": ans.Push(Math.Tanh(ans.Pop())); break;
-							case "exp": case "Exp": case "EXP": ans.Push(Math.Exp(ans.Pop())); break;
-							case "log": case "Log": case "LOG": ans.Push(Math.Log(ans.Pop())); break;
-						} break;
-					case TokenType.Brc: break;
-					default:
-						throw new ArgumentOutOfRangeException();
-				}
-				_expr.Enqueue(t);
-			}
+      for (var i = 0; i < _expr.Count; i++) {
+        var t = _expr.Dequeue();
+        switch(t.Identity){
+          case TokenType.Num: ans.Push(double.Parse(t.Buffer)); break;
+          case TokenType.Var:
+            if (vars?.ContainsKey(t.Buffer) ?? false) ans.Push(vars[t.Buffer]);
+            else throw new ArgumentOutOfRangeException("vars", "計算に必要な変数の値の情報が不足しています．");
+            break;
+          case TokenType.Opr:
+            var p = ans.Pop();
+            switch (t.Buffer) {
+              case "+": ans.Push(ans.Pop() + p); break;
+              case "-": ans.Push(ans.Pop() - p); break;
+              case "*": ans.Push(ans.Pop() * p); break;
+              case "/": ans.Push(ans.Pop() / p); break;
+              case "%": ans.Push(ans.Pop() % p); break;
+              case "^": ans.Push(Math.Pow(ans.Pop(), p)); break;
+              case "_": ans.Push(-p); break;
+            } break;
+          case TokenType.Cns:
+            switch(t.Buffer.ToUpper()) {
+              case "PI": ans.Push(Math.PI); break;
+              case "E": ans.Push(Math.E); break;
+              case "EPS": ans.Push(double.Epsilon); break;
+            } break;
+          case TokenType.Fnc:
+            switch(t.Buffer) {
+              case "sin": case "SIN": ans.Push(Math.Sin(ans.Pop())); break;
+              case "cos": case "COS": ans.Push(Math.Cos(ans.Pop())); break;
+              case "tan": case "TAN": ans.Push(Math.Tan(ans.Pop())); break;
+              case "Sin": ans.Push(Math.Asin(ans.Pop())); break;
+              case "Cos": ans.Push(Math.Acos(ans.Pop())); break;
+              case "Tan": ans.Push(Math.Atan(ans.Pop())); break;
+              case "sinh": case "Sinh": case "SINH": ans.Push(Math.Sinh(ans.Pop())); break;
+              case "cosh": case "Cosh": case "COSH": ans.Push(Math.Cosh(ans.Pop())); break;
+              case "tanh": case "Tanh": case "TANH": ans.Push(Math.Tanh(ans.Pop())); break;
+              case "exp": case "Exp": case "EXP": ans.Push(Math.Exp(ans.Pop())); break;
+              case "log": case "Log": case "LOG": ans.Push(Math.Log(ans.Pop())); break;
+            } break;
+          case TokenType.Brc: break;
+          default:
+            throw new ArgumentOutOfRangeException();
+        }
+        _expr.Enqueue(t);
+      }
 
-			return ans.Pop();
-		}
+      return ans.Pop();
+    }
 
-		/// <summary>
-		/// 変数を含んでいない数式の値を求める．
-		/// </summary>
-		public double Eval() {
-			return Eval(null);
-		}
+    /// <summary>
+    /// 変数を含んでいない数式の値を求める．
+    /// </summary>
+    public double Eval() {
+      return Eval(null);
+    }
 
-		/// <summary>
-		/// 数式を変更する．
-		/// </summary>
-		/// <param name="newExpression">変更したい数式を表す文字列．</param>
-		public void Change(string newExpression) {
-			_stringExp = newExpression;
-			_expr = Converter.Tokenize(newExpression.Replace(" ", ""));
-			Converter.ToRpn(_expr);
-		}
+    /// <summary>
+    /// 数式を変更する．
+    /// </summary>
+    /// <param name="newExpression">変更したい数式を表す文字列．</param>
+    public void Change(string newExpression) {
+      _stringExp = newExpression;
+      _expr = Converter.Tokenize(newExpression.Replace(" ", ""));
+      Converter.ToRpn(_expr);
+    }
 
-		/// <summary>
-		/// 数式をコンソールに表示する．
-		/// </summary>
-		public void Print() {
-			Console.WriteLine(_stringExp);
-		}
+    /// <summary>
+    /// 数式をコンソールに表示する．
+    /// </summary>
+    public void Print() {
+      Console.WriteLine(_stringExp);
+    }
 
-		/// <summary>
-		/// 逆ポーランド記法で表された数式の文字列を生成する．
-		/// </summary>
-		/// <returns>逆ポーランド記法で表された数式の文字列．</returns>
-		public override string ToString() {
-			var sb = new StringBuilder();
+    /// <summary>
+    /// 逆ポーランド記法で表された数式の文字列を生成する．
+    /// </summary>
+    /// <returns>逆ポーランド記法で表された数式の文字列．</returns>
+    public override string ToString() {
+      var sb = new StringBuilder();
 
-			sb.Clear();
-			for (var i = 0; i < _expr.Count; sb.Append(++i < _expr.Count ? " " : "")) {
-				var t = _expr.Dequeue();
-				sb.Append(t.Buffer + " ");
-				_expr.Enqueue(t);
-			}
+      sb.Clear();
+      for (var i = 0; i < _expr.Count; sb.Append(++i < _expr.Count ? " " : "")) {
+        var t = _expr.Dequeue();
+        sb.Append(t.Buffer + " ");
+        _expr.Enqueue(t);
+      }
 
-			return sb.ToString().TrimEnd();
-		}
-	}
+      return sb.ToString().TrimEnd();
+    }
+  }
 }
