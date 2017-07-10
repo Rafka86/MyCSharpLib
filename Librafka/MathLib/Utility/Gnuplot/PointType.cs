2484b6b73ad28e4fb7538b4bd64b2e8158bda059
Rafka <fw.ge-mu.930@jb4.so-net.ne.jp>
2017-07-10 17:15:12 +0900

    Make enum of pointtype.

---
+++ Librafka/MathLib/Utility/Gnuplot/PointType.cs
@@ -0,0 +1,20 @@
+﻿namespace Librafka.MathLib.Utility {
+  public enum PointType {
+    Dot,
+    Plus,
+    Cross,
+    Star,
+    Square,
+    FilledSquare,
+    Circle,
+    FilledCircle,
+    Triangle,
+    FilledTriangle,
+    InversedTriangle,
+    FilledInversedTriangle,
+    Rhombus,
+    FilledRhombus,
+    Pentagon,
+    FilledPentagon
+  }
+}
