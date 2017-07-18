97ca1cb2a1b2a3c85598f5961113b70d76293e1b
Rafka86 <fw.ge-mu.930@jb4.so-net.ne.jp>
2017-07-18 17:47:21 +0900

    WIP of plotstyle constructor.

--- Librafka/MathLib/Utility/Gnuplot/PlotStyle/PlotStyleBase.cs
+++ Librafka/MathLib/Utility/Gnuplot/PlotStyle/PlotStyleBase.cs
@@ -1,17 +1,32 @@
 ﻿namespace Librafka.MathLib.Utility {
+  /// <summary>
+  /// プロットスタイルを指定する構造体．
+  /// Gnuplotのlinetypeに対応する．
+  /// </summary>
   public partial struct PlotStyle {
+    /// <summary>
+    /// インスタンスの示す線の種類のプロパティ．
+    /// </summary>
     public LineType LineType { get; set; }
+    
+    /// <summary>
+    /// インスタンスの示す線の色のプロパティ．
+    /// </summary>
     public PlotColor PlotColor { get; set; }
+    
+    /// <summary>
+    /// インスタンスの示す線の幅のプロパティ．
+    /// </summary>
     public uint LineWidth { get; set; }
+    
+    /// <summary>
+    /// インスタンスの示す点の種類のプロパティ．
+    /// </summary>
     public PointType PointType { get; set; }
+    
+    /// <summary>
+    /// インスタンスの示す点の大きさのプロパティ．
+    /// </summary>
     public uint PointSize { get; set; }
-
-    public void Initialize() {
-      LineType = LineType.Solid;
-      PlotColor = PlotColor.Red;
-      LineWidth = 1;
-      PointType = PointType.FilledCircle;
-      PointSize = 1;
-    }
   }
 }
--- Librafka/MathLib/Utility/Gnuplot/PlotStyle/PlotStyleConstructor.cs
+++ Librafka/MathLib/Utility/Gnuplot/PlotStyle/PlotStyleConstructor.cs
@@ -1,13 +1,53 @@
 ﻿namespace Librafka.MathLib.Utility {
   public partial struct PlotStyle {
-    public PlotStyle(LineType lt) : this() {
-      LineType = lt;
+    /// <summary>
+    /// 線の種類を指定するコンストラクタ．
+    /// それ以外の要素はデフォルトの値が使用される．
+    /// </summary>
+    /// <param name="lt">線の種類．</param>
+    public PlotStyle(LineType lt) : this(lt, PlotColor.Red, 1, PointType.Dot, 1) {
     }
 
-    public PlotStyle(PlotColor pc) {
-      LineType = LineType.Solid;
+    /// <summary>
+    /// 線の色を指定するコンストラクタ．
+    /// それ以外の要素はデフォルトの値が使用される．
+    /// </summary>
+    /// <param name="pc">線の色．</param>
+    public PlotStyle(PlotColor pc) : this(LineType.Solid, pc, 1, PointType.Dot, 1) {
+    }
+
+    /// <summary>
+    /// 点の種類を指定するコンストラクタ．
+    /// それ以外の要素はデフォルトの値が使用される．
+    /// </summary>
+    /// <param name="pt">点の種類．</param>
+    public PlotStyle(PointType pt) : this(LineType.Solid, PlotColor.Red, 1, pt, 1) {
+    }
+
+    /// <summary>
+    /// 線の色と幅を指定するコンストラクタ．
+    /// </summary>
+    /// <param name="pc">線の色．</param>
+    /// <param name="lw">線の幅．</param>
+    public PlotStyle(PlotColor pc, uint lw) : this(LineType.Solid, pc, lw, PointType.Dot, 1) {
+    }
+
+    public PlotStyle(){}
+    
+    /// <summary>
+    /// プロットのスタイルを完全指定するコンストラクタ．
+    /// </summary>
+    /// <param name="lt">線の種類．</param>
+    /// <param name="pc">プロットの色．</param>
+    /// <param name="lw">線の幅．</param>
+    /// <param name="pt">点の種類．</param>
+    /// <param name="ps">点の大きさ．</param>
+    public PlotStyle(LineType lt, PlotColor pc, uint lw, PointType pt, uint ps) {
+      LineType = lt;
       PlotColor = pc;
-      LineWidth = 1;
+      LineWidth = lw;
+      PointType = pt;
+      PointSize = ps;
     }
   }
 }
